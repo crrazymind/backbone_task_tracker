d0466dd543141e0abf59581af1730bd84751f794
Crazymind <crazymind2004@gmail.com>
2013-01-17 19:14:44 +0200

    login implemented

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ accTypeHolder = dict()
 for item in accTypes:
     accTypeHolder[md5.new(item).digest()] = item
 
+userStack = dict({"200820e3227815ed1756a6b531e7e0d2": {"usertype": "user", "userid": "1", "username": "qwe", "pwd": "202cb962ac59075b964b07152d234b70"}})
+
 sessionHolder = dict({
     "7f6ef71197989eaf11afe2cc588273d7": {"usertype": "admin", "userid": "0", "username": "ololo", "pwd": "202cb962ac59075b964b07152d234b70"},
     "e638ce6fdd2502f88962460002775972": {"usertype": "user", "userid": "1", "username": "noob", "pwd": "202cb962ac59075b964b07152d234b70"}
@@ -60,7 +62,7 @@ def session():
     return bottle.template("index", user=user)
 
 
-@get('/loginme')
+@get('/loginme11')
 def login():
     user = auth.checkUser(request)
     auth.initUser(request, response, "admin")
@@ -78,17 +80,12 @@ def login():
 
 @post('/sendCred')
 def login():
-    username = request.forms.get("login")
-    pwd = request.forms.get("password")
-    user = dict()
-    print "__________", pwd, username
-    user["usertype"] = "user"
-    user["username"] = username
-    user["userid"] = md5.new(username + pwd).digest()
-    user["pwd"] = md5.new(pwd).digest()
-    #{"usertype": "admin", "userid": "1234567899"}
-    auth.initUser(request, response, user)
-    bottle.redirect("/session")
+    user = auth.getUser()
+    if auth.checkUser(user):
+        auth.initUser(request, response, user)
+        bottle.redirect("/session")
+    else:
+        bottle.redirect("/login")
 
 
 @get('/login')
@@ -114,16 +111,21 @@ class tryAuth():
     def prepare():
         print self.db
 
-    def getUser(self, user, request, response):
+    def getUser123(self, user, request, response):
         if self.validateUser(user, user):
             return dict({"usertype": "admin", "userid": "1234567899"})
         else:
             return user
 
-    def validateUser(self, login, pwd):
-        return True
+    def validateUser(self, user):
+        #user.username
+        bduser = self.db.session.find_one({"userid": md5.new(user["username"] + user["pwd"]).digest()})
+        print dbuser
+        if bduser:
+            return True
 
     def initUser(self, request, response, user):
+        #self.checkUser()
         sessionHolder[user["userid"]] = user
         response.set_cookie("account", md5.new(user["usertype"]).digest(), secret=passPhrase)
         response.set_cookie("userName", md5.new(user["username"]).digest(), secret=passPhrase)
@@ -132,11 +134,23 @@ class tryAuth():
     def logoffUser(self, request, response):
         response.set_cookie("account", "", secret=passPhrase)
 
-    def checkUser1(self, request):
-        username = request.get_cookie("userName", secret=passPhrase)
-        return "ololo"
+    def checkUser(self, user):
+        if self.validateUser(user):
+            return True
+        else:
+            return False
 
-    def checkUser(self, request):
+    def getUser(self):
+        username = request.forms.get("login")
+        pwd = request.forms.get("password")
+        user = dict({"usertype": "", "username": "", "userid": "", "pwd": ""})
+        user["usertype"] = "user"
+        user["username"] = username
+        user["userid"] = md5.new(username + pwd).digest()
+        user["pwd"] = md5.new(pwd).digest()
+        return user
+
+    def checkUser1(self, request):
         username = request.get_cookie("account", secret=passPhrase)
         if username:
             if accTypeHolder[username]:
